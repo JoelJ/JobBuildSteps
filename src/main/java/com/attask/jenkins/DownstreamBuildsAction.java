43d9140ea5968eed2bde10fcbf413c335135e4a9
Joel <johnson.joel.86@gmail.com>
2013-03-06 18:15:20 -0700

    adding a summary section to the build page listing all the downstream jobs.

--- src/main/java/com/attask/jenkins/DownstreamBuildsAction.java
+++ src/main/java/com/attask/jenkins/DownstreamBuildsAction.java
@@ -1,7 +1,12 @@
 package com.attask.jenkins;
 
 import hudson.model.Action;
+import hudson.model.BallColor;
+import hudson.model.Result;
 import hudson.model.Run;
+import hudson.tasks.test.AbstractTestResultAction;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,6 +17,7 @@ import java.util.List;
  * Date: 3/6/13
  * Time: 4:32 PM
  */
+@ExportedBean
 public class DownstreamBuildsAction implements Action {
 	private final List<String> downstreamBuilds;
 
@@ -25,8 +31,39 @@ public class DownstreamBuildsAction implements Action {
 		}
 	}
 
+	@Exported
 	public List<String> getDownstreamBuilds() {
 		return Collections.unmodifiableList(downstreamBuilds);
+	}
+
+	public List<Run> findDownstreamBuilds() {
+		List<Run> result = new ArrayList<Run>(downstreamBuilds.size());
+		for (String downstreamBuildId : downstreamBuilds) {
+			Run<?, ?> run = Run.fromExternalizableId(downstreamBuildId);
+			if(run != null) {
+				result.add(run);
+			}
+		}
+		return result;
+	}
+
+	public String findOrb(Run run) {
+		boolean building = run.isBuilding();
+		if(building) {
+			return BallColor.NOTBUILT_ANIME.getImageOf("16x16");
+		} else {
+			Result result = run.getResult();
+			return result.color.getImageOf("16x16");
+		}
+	}
+
+	public Integer findFailureCount(Run run) {
+		Integer result = null;
+		AbstractTestResultAction testAction = run.getAction(AbstractTestResultAction.class);
+		if(testAction != null) {
+			result = testAction.getFailCount();
+		}
+		return result;
 	}
 
 	public String getIconFileName() {
@@ -38,6 +75,6 @@ public class DownstreamBuildsAction implements Action {
 	}
 
 	public String getUrlName() {
-		return null;
+		return "downstreamBuilds";
 	}
 }
